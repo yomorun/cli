00a96f03dc3485c77bc2f06fbbccfa634b2c8472
Hong Xiaojian <xiaojian.hong@gmail.com>
2021-10-20 19:53:24 +0800

    feat: add `yomo demo list`

--- cmd/demo.go
+++ cmd/demo.go
@@ -28,7 +28,7 @@ import (
 	"github.com/yomorun/cli/serverless"
 )
 
-// demoCmd represents the dev command
+// demoCmd represents the demo command
 var demoCmd = &cobra.Command{
 	Use:                "demo",
 	Short:              "Run YoMo demos",
@@ -38,46 +38,8 @@ var demoCmd = &cobra.Command{
 		if len(args) > 0 {
 			opts.Filename = args[0]
 		}
-		// Serverless
-		log.InfoStatusEvent(os.Stdout, "YoMo Stream Function file: %v", opts.Filename)
-		if isUrl(opts.Filename) {
-			// download file
-		  log.PendingStatusEvent(os.Stdout, "Downloading file...")
-			tempFile, err := download(opts.Filename)
-			if err != nil {
-				log.FailureStatusEvent(os.Stdout, err.Error())
-				return
-			}
-			opts.Filename = tempFile
-		}
 
-		// resolve serverless
-		log.PendingStatusEvent(os.Stdout, "Create YoMo Stream Function instance...")
-
-		// Connect the serverless to YoMo dev-server, it will automatically emit the mock data.
-		opts.Host = "dev.yomo.run"
-		opts.Port = 9201
-		opts.Name = "basic"
-
-		s, err := serverless.Create(&opts)
-		if err != nil {
-			log.FailureStatusEvent(os.Stdout, err.Error())
-			return
-		}
-
-		// build
-		log.PendingStatusEvent(os.Stdout, "YoMo Stream Function building...")
-		if err := s.Build(true); err != nil {
-			log.FailureStatusEvent(os.Stdout, err.Error())
-			return
-		}
-		log.SuccessStatusEvent(os.Stdout, "Success! YoMo Stream Function build.")
-		// run
-		log.InfoStatusEvent(os.Stdout, "YoMo Stream Function is running...")
-		if err := s.Run(verbose); err != nil {
-			log.FailureStatusEvent(os.Stdout, err.Error())
-			return
-		}
+		runDemo()
 	},
 }
 
@@ -85,6 +47,49 @@ func init() {
 	rootCmd.AddCommand(demoCmd)
 
 	demoCmd.Flags().StringVarP(&opts.Filename, "file", "f", "app.go", "The URL or local path of Stream function file")
+}
+// runDemo runs a YoMo demo.
+func runDemo() {
+	// Serverless
+	log.InfoStatusEvent(os.Stdout, "YoMo Stream Function file: %v", opts.Filename)
+	if isUrl(opts.Filename) {
+		// download file
+		log.PendingStatusEvent(os.Stdout, "Downloading file...")
+		tempFile, err := download(opts.Filename)
+		if err != nil {
+			log.FailureStatusEvent(os.Stdout, err.Error())
+			return
+		}
+		opts.Filename = tempFile
+	}
+
+	// resolve serverless
+	log.PendingStatusEvent(os.Stdout, "Create YoMo Stream Function instance...")
+
+	// Connect the serverless to YoMo dev-server, it will automatically emit the mock data.
+	opts.Host = "dev.yomo.run"
+	opts.Port = 9201
+	opts.Name = "basic"
+
+	s, err := serverless.Create(&opts)
+	if err != nil {
+		log.FailureStatusEvent(os.Stdout, err.Error())
+		return
+	}
+
+	// build
+	log.PendingStatusEvent(os.Stdout, "YoMo Stream Function building...")
+	if err := s.Build(true); err != nil {
+		log.FailureStatusEvent(os.Stdout, err.Error())
+		return
+	}
+	log.SuccessStatusEvent(os.Stdout, "Success! YoMo Stream Function build.")
+	// run
+	log.InfoStatusEvent(os.Stdout, "YoMo Stream Function is running...")
+	if err := s.Run(verbose); err != nil {
+		log.FailureStatusEvent(os.Stdout, err.Error())
+		return
+	}
 }
 
 // isUrl validates if the filename is a URL.
---
+++ cmd/demo_list.go
@@ -0,0 +1,90 @@
+/*
+Copyright © 2021 CELLA, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
+)
+
+// demoListCmd represents the list sub command in demo.
+var demoListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List the examples in YoMo.",
+	Long:  "List the Streaming Serverless and Geo-Distributed examples provided by YoMo.",
+	Run: func(cmd *cobra.Command, args []string) {
+		selectDemo()
+	},
+}
+
+func init() {
+	demoCmd.AddCommand(demoListCmd)
+}
+
+type promptContent struct {
+	errorMsg string
+	label    string
+}
+
+// selectDemo selects a demo and run it in local.
+func selectDemo() {
+	selectDemoContent := promptContent{
+		"Please select a demo.",
+		"Select a demo which you'd like to run",
+	}
+	index := promptGetDemoSelect(selectDemoContent)
+	if index < 0 {
+		fmt.Println("Invalid index", index)
+		os.Exit(1)
+	}
+
+	switch index {
+	case 0:
+		opts.Filename = "https://play.yomo.run/static/demosfn/basic.go"
+		runDemo()
+	default:
+		fmt.Println("TODO: add this demo.")
+	}
+
+}
+
+func promptGetDemoSelect(pc promptContent) int {
+	items := []string{"Streaming Serverless", "Customized yomo-source", "Geo-Distributed Streaming", "IoT Streaming"}
+	index := -1
+	var result string
+	var err error
+
+	for index < 0 {
+		prompt := promptui.Select{
+			Label: pc.label,
+			Items: items,
+		}
+
+		index, result, err = prompt.Run()
+	}
+
+	if err != nil {
+		fmt.Println("Prompt failed", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Select the demo:", result)
+
+	return index
+}
